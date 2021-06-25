5cc8116fe0711378eabb35048938711675e1e0e4
iamlambda <77165319+iamlambda@users.noreply.github.com>
2021-06-25 20:59:43 +0200

    Update monitors.sh

--- monitors.sh
+++ monitors.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --mode 1600x900 --pos 0x0 --rotate normal --output HDMI-0 --off --output DP-0 --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output DP-1 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off
 xrandr --output DP-0 --mode 1920x1080 --rate 144.00
+xrandr --output HDMI-0 --mode 1920x1080 --rate 75
